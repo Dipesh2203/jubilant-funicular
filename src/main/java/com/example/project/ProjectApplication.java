64a544899537a9cbe700dccf040ed56d5b74ef59
Dipesh Kumar <dipesh.shs11@gmail.com>
2026-02-17 09:32:01 +0530

    Add imports for Map, HashMap, and List

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -10,6 +10,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+import java.util.HashMap;
+import java.util.List;
+
 
 @SpringBootApplication
 @RestController
